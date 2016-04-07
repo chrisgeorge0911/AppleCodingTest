f506bd4c45b5da1da1bc40f267771b85feee218b
chris <chris@chrisgeorge.org.uk>
2016-04-07 20:24:08 +0100

    first pass at the word parser using the new trie code

--- interpreter.py
+++ interpreter.py
@@ -1,7 +1,29 @@
+from concepts import Concepts
+
 
 class Interpreter:
 
-    def getMatchesForString(self, sentence):
+    def get_matches_for_string(self, sentence):
 
-        return {sentence}
+        concepts = Concepts()
+
+        matched_list = []
+
+        words = sentence.lower().split(' ')
+
+        for i in range(0,len(words)):
+
+            if concepts.first_letter_match(words[i]):
+
+                for j in range(i, len(words)):
+
+                    # print("word: {}".format(words[i]))
+                    joined_word = ' '.join(words[i:j+1])
+
+                    # print("joined word: {}".format(joined_word))
+
+                    if concepts.match(joined_word):
+                        matched_list.append(joined_word)
+
+        return matched_list
 
--- test_interpreter.py
+++ test_interpreter.py
@@ -8,16 +8,17 @@ class TestInterpreter(TestCase):
 
         interpreter = Interpreter()
 
-        self.assertEqual(interpreter.getMatchesForString('I would like some thai food'), {'thai'})
-        self.assertEqual(interpreter.getMatchesForString('Where can I find good sushi'), {'sushi'})
-        self.assertEqual(interpreter.getMatchesForString('Find me a place that does tapas'), {})
-        self.assertEqual(interpreter.getMatchesForString('Which restaurants do East Asian food'), {'east asian'})
-        self.assertEqual(interpreter.getMatchesForString('Which restaurants do West Indian food'), {'west indian', 'indian'})
-        self.assertEqual(interpreter.getMatchesForString('What is the weather like today'), {})
+        self.assertEqual(interpreter.get_matches_for_string('I would like some thai food'), ['thai'])
+        self.assertEqual(interpreter.get_matches_for_string('Where can I find good sushi'), ['sushi'])
+        self.assertEqual(interpreter.get_matches_for_string('Find me a place that does tapas'), [])
+        self.assertEqual(interpreter.get_matches_for_string('Which restaurants do East Asian food'), ['east asian'])
+        self.assertEqual(interpreter.get_matches_for_string('Which restaurants do West Indian food'), ['west indian', 'indian'])
+        self.assertEqual(interpreter.get_matches_for_string('What is the weather like today'), [])
+
 
     def test_getMatchesForString_emptyString(self):
 
         interpreter = Interpreter()
 
-        self.assertEqual(interpreter.getMatchesForString(''), {})
+        self.assertEqual(interpreter.get_matches_for_string(''), [])
 
