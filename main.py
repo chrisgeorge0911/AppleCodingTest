76ed985ca05aa29a922a71bf677134402a42819e
chris <chris@chrisgeorge.org.uk>
2016-04-07 23:02:26 +0100

    Added main.py to run from command line

---
+++ main.py
@@ -0,0 +1,19 @@
+import argparse
+
+from interpreter import Interpreter
+
+
+def main(**kwargs):
+
+    interpreter = Interpreter()
+
+    results = interpreter.get_matches_for_string(kwargs['sentence'][0])
+
+    print( ', '.join(results))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Enter a sentence and get a list of concepts')
+    parser.add_argument('\"sentence\"', nargs='+', type=str, help='Enter a sentence')
+    args = parser.parse_args()
+    main(**vars(args))
